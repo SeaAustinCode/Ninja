ad222af737d60ca88b0d28afadba45bfa37a6725
SeaAustinCode <seaaustincode@gmail.com>
2022-05-02 19:06:00 -0700

    Super_Ninja

---
+++ car-test.js
@@ -0,0 +1,13 @@
+// parent Vehicle class
+class Vehicle {
+    constructor(manufacturer, model, color) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.color = color;
+        this.miles = 0;
+    }
+    drive() {
+        this.miles += 10;
+        console.log(`You drove your ${ this.constructor.name } and it now has ${this.miles} miles on it.`);
+    }
+}
--- ninja.js
+++ ninja.js
@@ -1,9 +1,9 @@
 class Ninja {
-    constructor(name, health, strength = 3, speed = 3) {
+    constructor(name) {
         this.name = name;
-        this.health = health;
-        this.speed = speed;
-        this.strength = strength;
+        this.health = 100;
+        this.speed = 3;
+        this.strength = 3;
     }
 
     sayName() {
@@ -23,3 +23,24 @@ const ninja1 = new Ninja("Donatello", 100);
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
+
+class Sensei extends Ninja {
+    constructor(name) {
+        super(name);
+        this.health = 200;
+        this.strength = 10;
+        this.speed = 10;
+        this.wisdom = 10;
+    }
+    speakWisdom(){
+        super.drinkSake()
+        console.log("What one programmer can do in one month, two programmers can do in two months.")
+    }
+}
+
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
